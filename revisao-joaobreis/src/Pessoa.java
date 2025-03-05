f8b306c83e9e9838a60273595355219404692310
vbreiis <pvd.breis@gmail.com>
2025-03-05 20:27:32 -0300

    feat: revisao

---
+++ revisao-joaobreis/src/Pessoa.java
@@ -0,0 +1,13 @@
+public class Pessoa {
+    private String nome; //variavel/atributo
+
+    //metodo
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
