3b928c14ac244b3ca8942d9aa8b19258742308c4
kleberws <kleberwsff@gmail.com>
2025-03-26 19:54:03 -0300

    feat: JFrame v2

--- sistema_eventos/src/main/java/br/univille/view/FrmMain.java
+++ sistema_eventos/src/main/java/br/univille/view/FrmMain.java
@@ -1,14 +1,20 @@
 package br.univille.view;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+
 public class FrmMain extends JFrame {
 
     private JLabel lblId, lblNome, lblEndereco;
     private JTextField txtId, txtNome, txtEndereco;
+    private JButton btnSalvar, btnSair;
+
 
     public FrmMain(){
         setSize(500, 500);
@@ -16,12 +22,26 @@ public class FrmMain extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         painelCampos();
+        painelBotoes();
 
         setVisible(true);
     }
 
+    private void painelBotoes(){
+        JPanel painel = new JPanel();
+        painel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        btnSalvar = new JButton("Salvar");
+        painel.add(btnSalvar);
+        
+        btnSair = new JButton("Sair");
+        painel.add(btnSair);
+        getContentPane().add(painel,"South");
+    }
+
     private void painelCampos(){
         JPanel painel = new JPanel();
+        painel.setLayout(new GridLayout(3,2));
         lblId = new JLabel("ID");
         painel.add(lblId);
         txtId = new JTextField(5);
@@ -37,6 +57,7 @@ public class FrmMain extends JFrame {
         txtEndereco = new JTextField(15);
         painel.add(txtEndereco);
 
-        getContentPane().add(painel);
+        getContentPane().add(painel, "North");
     }
+    
 }
